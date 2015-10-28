2f530369928c29c9e5514f9f5ed341adaf38d2b8
ljtale <ljtale@gmail.com>
2015-10-27 23:03:48 -0500

    all the source files

---
+++ include/config.h
@@ -0,0 +1,8 @@
+/* Automatically generated - do not edit */
+#define CONFIG_NAND	1
+#define CONFIG_BOARDDIR board/ti/am335x
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/am335x_evm.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ include/generated/timestamp_autogenerated.h
@@ -0,0 +1,3 @@
+#define U_BOOT_DATE "Oct 27 2015"
+#define U_BOOT_TIME "22:58:06"
+#define U_BOOT_TZ "-0500"
